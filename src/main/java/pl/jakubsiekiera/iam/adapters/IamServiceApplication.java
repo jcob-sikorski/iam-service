1cec18bd9ec52511abbda5bb065c2cb05273afa7
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-01-27 16:23:05 +0100

    Remove IamServiceApplication class and enhance application.properties with detailed configuration settings for application metadata, server configuration, logging, and optional database logging.

--- src/main/java/pl/jakubsiekiera/iam/adapters/IamServiceApplication.java
+++ src/main/java/pl/jakubsiekiera/iam/adapters/IamServiceApplication.java
@@ -1,4 +1,4 @@
-package pl.jakubsiekiera.iam;
+package pl.jakubsiekiera.iam.adapters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
